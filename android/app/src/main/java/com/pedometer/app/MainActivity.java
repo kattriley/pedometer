f1deda0b2b5a5538d8ac24448ef17e77e4bd66e5
kattriley <kattriley@users.noreply.github.com>
2026-07-06 17:42:19 +0200

    Switch to manual Android project (no Capacitor)

---
+++ android/app/src/main/java/com/pedometer/app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.pedometer.app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(false);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
